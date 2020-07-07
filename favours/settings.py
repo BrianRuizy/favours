64ea360defb0db2137a19f70700c750fa86dd127
Brian <brianruiz123@yahoo.com>
2020-07-07 00:00:26 -0500

    crispy forms usage

--- favours/settings.py
+++ favours/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'crispy_forms', 
-    
+    'bootstrap_modal_forms',
+
     # custom apps go here...
     'favours.apps.accounts',
 ]
