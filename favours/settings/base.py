aba7fb9564a9b2a0467d2b0cef711c0bd6cc6b93
Brian <brianruiz123@yahoo.com>
2020-07-10 00:40:51 -0500

    seperate settings respective to env

--- favours/settings/base.py
+++ favours/settings/base.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'favours.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'favours/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -151,7 +151,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "favours/static"),
+    os.path.join(BASE_DIR, 'static'),
+    
 ]
 
 
