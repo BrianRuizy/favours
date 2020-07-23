b853b31cab5c51c1a4eba29fbac3ff243f204714
Brian <brianruiz123@yahoo.com>
2020-07-22 23:19:38 -0500

    added method to compress user profile images

--- favours/apps/users/models.py
+++ favours/apps/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):    
@@ -8,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
