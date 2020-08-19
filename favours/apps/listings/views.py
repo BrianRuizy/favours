72c2fc6512535e57c58b8e43e42c99a23bcfe655
Brian <brianruiz123@yahoo.com>
2020-08-19 15:04:19 -0500

    added paginaton to posts

--- favours/apps/listings/views.py
+++ favours/apps/listings/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 3
 
 
 class PostDetailView(DetailView):
