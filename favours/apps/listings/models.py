c61274550dd942064982bd841dd81088b3a2855b
Brian <brianruiz123@yahoo.com>
2020-08-22 20:33:31 -0500

    flake 8 formatting

--- favours/apps/listings/models.py
+++ favours/apps/listings/models.py
@@ -3,16 +3,16 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         # Sets where to redirect user after submitting a post
         return reverse('post-detail', kwargs={"pk": self.pk})
-    
--- favours/apps/listings/urls.py
+++ favours/apps/listings/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView, 
+    PostDeleteView, 
+    UserPostListView
+)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='listings-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
