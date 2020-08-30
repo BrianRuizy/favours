1d31fe9a2c050bc2a873d65fcb2e32021549c49c
Brian <brianruiz123@yahoo.com>
2020-08-30 08:00:16 -0500

    models fix

--- favours/apps/users/models.py
+++ favours/apps/users/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
-        super().save()
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
