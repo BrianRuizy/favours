2febeb61c487567f7917dcc108b9e6db1e9db4d0
Brian <brian.ruiz@periship.com>
2020-09-16 23:25:43 -0500

    moved templates folder, modified navbar

--- favours/settings/base.py
+++ favours/settings/base.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'favours.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
