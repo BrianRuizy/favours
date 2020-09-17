7b72f00fa570121013e860d5dc5e18d461b06c93
Brian <brianruiz123@yahoo.com>
2020-09-17 01:20:07 -0500

    completed API views refactoring

--- apps/api/views.py
+++ apps/api/views.py
@@ -17,8 +17,8 @@ def post_list(request):
     """ Read all posted favours,
     or post a new favour if valid post request """
     if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
+        all_posts = Post.objects.all()
+        serializer = PostSerializer(all_posts, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
@@ -28,18 +28,25 @@ def post_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @api_view('GET', 'PUT', 'DELETE')
 def post_detail(request, pk):
     """ Read, update, or delete a specific posted favour """
     try:
-        post = Post.objects.get(pk=pk)
+        specific_post = Post.objects.get(pk=pk)
     except Post.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        pass
+        serializer = PostSerializer(specific_post)
+        return Response(serializer.data)
+
     elif request.method == 'PUT':
-        pass
-    else request.method == 'DELETE':
-        pass
+        serializer = PostSerializer(specific_post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        specific_post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
