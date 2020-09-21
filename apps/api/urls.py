11fdac5168588650673c43f8363b60b2fb4d204a
Brian <brianruiz123@yahoo.com>
2020-09-21 00:17:28 -0500

    implemented generic API class-based views

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('api/posts/', views.post_list, name='api-post-list'),
-    path('api/posts/<int:pk>', views.post_detail, name='api-post-details'),
+    path('api/posts/', views.PostList.as_view()),
+    path('api/posts/<int:pk>', views.PostDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,52 +1,17 @@
-from rest_framework import generics, filters, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from apps.listings.models import Post
 from .serializers import PostSerializer
 
 
-# class PostList(generics.ListCreateAPIView):
-#     search_fields = ['title']
-#     filter_backends = (filters.SearchFilter, )  # basic search functionality
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-
-@api_view(['GET', 'POST'])
-def post_list(request, format=None):
-    """ Read all posted favours,
+class PostList(generics.ListCreateAPIView):
+    """ Reads all posted favours,
     or post a new favour if valid post request """
-    if request.method == 'GET':
-        all_posts = Post.objects.all()
-        serializer = PostSerializer(all_posts, many=True)
-        return Response(serializer.data)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    elif request.method == 'POST':
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, pk, format=None):
-    """ Read, update, or delete a specific posted favour """
-    try:
-        specific_post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = PostSerializer(specific_post)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = PostSerializer(specific_post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        specific_post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """ Read, update, or delete a specific posted favou """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
